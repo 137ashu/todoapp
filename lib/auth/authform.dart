08cd4636c3efe322b63f1871ccab580e17f4c4f8
Ashutosh Thite <137ashu@gmail.com>
2022-08-24 15:16:23 +0530

    some changes

--- lib/auth/authform.dart
+++ lib/auth/authform.dart
@@ -1,4 +1,8 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AuthForm extends StatefulWidget {
   // const AuthForm({Key? key}) : super(key: key);
@@ -19,16 +23,28 @@ class _AuthFormState extends State<AuthForm> {
       width: MediaQuery.of(context).size.width,
       child: ListView(
         children:[
+          // Container(
+          //     margin: EdgeInsets.all(30),
+          //     height: 200,
+          //     child: Image.asset('assets/todo.png'),
+          //   ),
           Container(
+            padding: EdgeInsets.only(left: 10, right:10, top:10),
             child: Form(
               key: _formkey,
-            )
+            ),
             child: Column(
               children: [
                 TextFormField(
-                  keyboardType: TextInputType.emailAddress;
+                  keyboardType: TextInputType.emailAddress,
                   key: ValueKey('email'),
-                )
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(8.0),
+                      borderSide: new BorderSide()),
+                      labelText: "Enter Email",
+                      labelStyle: google_fonts.roboto()),
+                ),
               ],
             )
           )
