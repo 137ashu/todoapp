e53107728b031d7a91ca375684578a558f1638b0
Ashutosh Thite <137ashu@gmail.com>
2022-08-24 12:15:41 +0530

    adding some files

---
+++ lib/auth/authform.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  // const AuthForm({Key? key}) : super(key: key);
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formkey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: ListView(
+        children:[
+          Container(
+            child: Form(
+              key: _formkey,
+            )
+            child: Column(
+              children: [],
+            )
+          )
+        ]
+      )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp2/auth/authscreen.dart';
 import 'package:todoapp2/screens/home.dart';
 
 void main()=>runApp(new myApp());
@@ -8,7 +9,7 @@ class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      home: AuthScreen(),
       debugShowCheckedModeBanner: false,
       theme:
         ThemeData(brightness: Brightness.dark, primaryColor:Colors.purple)
